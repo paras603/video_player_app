037630298d15eb4b6f64d6f6904a3ed5fa0447b3
paras603 <parasmanirai1@gmail.com>
2023-08-09 17:34:36 +0545

    videop playing from external source

--- app/src/main/java/com/mastercoding/videoplayer/MainActivity.kt
+++ app/src/main/java/com/mastercoding/videoplayer/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.mastercoding.videoplayer
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Media
 import android.widget.MediaController
 import android.widget.VideoView
 
@@ -20,8 +22,20 @@ class MainActivity : AppCompatActivity() {
         mediaController.setMediaPlayer(videoView)
         videoView.setMediaController(mediaController)
 
-        videoView.start()
 
+
+        //displaying video from internet
+        val videoView2 = findViewById<VideoView>(R.id.videoView2)
+
+        val uri = Uri.parse("https://static.videezy.com/system/resources/previews/000/037/813/original/WH038.mp4")
+        videoView2.setVideoURI(uri)
+
+        val mediaController2 = MediaController(this)
+        mediaController2.setAnchorView(videoView2)
+        mediaController2.setMediaPlayer(videoView2)
+        videoView2.setMediaController(mediaController2)
+
+        videoView2.start()
 
 
 
